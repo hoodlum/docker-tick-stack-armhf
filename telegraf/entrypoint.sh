221174322d903bc3c501554c066311a1d42c9cd8
hoodlum <sst77@gmx.de>
2017-08-26 14:18:39 +0000

    Refactor form multi-stage build

--- telegraf/entrypoint.sh
+++ telegraf/entrypoint.sh
@@ -1,2 +1,13 @@
-sds
+#!/bin/sh
+set -e
 
+if [ "${1}" = 'telegraf' ]; then
+    set -- /usr/bin/telegraf "$@"
+fi
+
+if [ "${1}" = 'config' ]; then
+    set -- /usr/bin/telegraf "$@"
+fi
+
+
+exec "$@"
