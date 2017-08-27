bcf44dab855bb7fdcfe1531dbf26c3caf0e84d78
hoodlum <sst77@gmx.de>
2017-08-27 18:36:51 +0000

    Add entrypoint script for influxdb

--- influxdb/entrypoint.sh
+++ influxdb/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -e
 
-if [ "${1}" = 'telegraf' ]; then
-    set -- /usr/bin/telegraf "$@"
+if [ "${1}" = 'console' ]; then
+    set -- /usr/bin/influx "$@"
 fi
 
-if [ "${1}" = 'config' ]; then
-    set -- /usr/bin/telegraf "$@"
+if [ "${1}" = 'influxd' ]; then
+    set -- /usr/bin/influxd "$@"
 fi
 
 
