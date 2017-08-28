396bfbac5bb5c54578b443a40b7c029e22653efb
hoodlum <sst77@gmx.de>
2017-08-28 21:14:59 +0000

    modify telegraf entrypoint.sh

--- telegraf/entrypoint.sh
+++ telegraf/entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 set -e
 
-if [ "${1}" = 'telegraf' ]; then
-    set -- /usr/bin/telegraf "$@"
-fi
-
 if [ "${1}" = 'config' ]; then
-    set -- /usr/bin/telegraf "$@"
+    set -- telegraf "$@"
 fi
 
 
